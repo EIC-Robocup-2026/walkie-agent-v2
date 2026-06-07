b021be321bffacc0030e3f0c44dc272918f2f19d
armmy2530 <sutigran2557@gmail.com>
2026-06-07 21:11:37 +0700

    fix

--- test_get_tilt.py
+++ test_get_tilt.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 import os
 import sys
-from time import time
+import time
 
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
